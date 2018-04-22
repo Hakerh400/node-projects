43a07677f3b9f5d48e76ac04e92f08f111b5be60
Hakerh400 <hakerh403@gmail.com>
2018-04-22 05:38:21 +0200

    commit

--- neural-net/main.js
+++ neural-net/main.js
@@ -1,80 +1,66 @@
 'use strict';
 
-var fs = require('fs');
-var path = require('path');
 var O = require('../framework');
+var media = require('../media');
+var nn = require('.');
 
-var inputLayerSize = 1;
-var outputLayerSize = 10;
+const HD = 1;
 
-var hiddenLayersNum = 1;
-var neuronsPerLayer = 30;
+var w = HD ? 1920 : 640;
+var h = HD ? 1080 : 480;
+var fps = 60;
+var duration = 60;
+var framesNum = fps * duration;
 
-var neuralNetFile = path.join(__dirname, '../../Python/neural-net/output.txt');
+var speed = 5;
+
+var entRadius = 16;
+var gemRadius = 8;
 
 setTimeout(main);
 
 function main(){
-  var data = fs.readFileSync(neuralNetFile, 'utf8');
-  data = data.split(/[\s\[\]]+/).filter(a => a).map(a => +a);
+  var model = nn.loadModel();
 
-  var wss = [];
-  var bss = [];
+  var ent = new O.Vector(O.rand(w), O.rand(h));
+  var gem = new O.Vector(O.rand(w), O.rand(h));
 
-  var layers = [new Float32Array(inputLayerSize)];
+  media.renderVideo('-vid/1.mp4', w, h, fps, true, (w, h, g, f) => {
+    media.logStatus(f, framesNum);
 
-  wss.push(splice(inputLayerSize, neuronsPerLayer));
-  bss.push(data.splice(0, neuronsPerLayer));
+    draw(g);
+    tick();
 
-  for(var i = 0; i < hiddenLayersNum; i++){
-    layers.push(new Float32Array(neuronsPerLayer));
+    return f !== framesNum;
+  });
 
-    var nextLayerSize = i !== hiddenLayersNum - 1 ? neuronsPerLayer : outputLayerSize;
+  function tick(){
+    var input = [ent.x / w, ent.y / h, gem.x / w, gem.y / h];
+    var output = model.feed(input);
 
-    wss.push(splice(neuronsPerLayer, nextLayerSize));
-    bss.push(data.splice(0, nextLayerSize));
-  }
+    ent.x += (output[0] * 2 - 1) * speed;
+    ent.y += (output[1] * 2 - 1) * speed;
 
-  layers.push(new Float32Array(outputLayerSize));
-
-  /////////////////////////////////////////////////////////
-
-  var input = [0];
-  var output = calc(input);
-  console.log(output.map(a => a.toFixed(2)).join('\n'));
-
-  /////////////////////////////////////////////////////////
-
-  function calc(input){
-    input.forEach((a, i) => layers[0][i] = a);
-
-    for(var layerIndex = 1; layerIndex < layers.length; layerIndex++){
-      var ws = wss[layerIndex - 1];
-      var bs = bss[layerIndex - 1];
-
-      var layerPrev = layers[layerIndex - 1];
-      var layer = layers[layerIndex];
-
-      for(var neuronIndex = 0; neuronIndex < layer.length; neuronIndex++){
-        var sum = bs[neuronIndex];
-
-        for(var neuronPrevIndex = 0; neuronPrevIndex < layerPrev.length; neuronPrevIndex++)
-          sum += layerPrev[neuronPrevIndex] * ws[neuronPrevIndex][neuronIndex];
-
-        layer[neuronIndex] = Math.tanh(sum);
-      }
+    if(ent.dist(gem) < entRadius + gemRadius){
+      gem.x = O.rand(w);
+      gem.y = O.rand(h);
     }
-
-    return [...layers[layers.length - 1]];
   }
 
-  function splice(h, w){
-    var d = data.splice(0, w * h);
-    var arr = [];
+  function draw(g){
+    g.fillStyle = 'darkgray';
+    g.fillRect(0, 0, w, h);
 
-    for(var i = 0; i < h; i++)
-      arr.push(d.splice(0, w));
+    g.fillStyle = '#00ff00';
+    g.beginPath();
+    g.arc(ent.x, ent.y, entRadius, 0, O.pi2);
+    g.fill();
+    g.stroke();
 
-    return arr;
+    g.fillStyle = '#ffff00';
+    g.beginPath();
+    g.arc(gem.x, gem.y, gemRadius, 0, O.pi2);
+    g.fill();
+    g.stroke();
   }
 }
