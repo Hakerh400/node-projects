625b9568b75b7d554c541ab3971ade1e75fe019e
Hakerh400 <hakerh403@gmail.com>
2018-05-02 07:40:01 +0200

    commit

--- svg/index.js
+++ svg/index.js
@@ -2,8 +2,6 @@
 
 var O = require('../framework');
 
-const OFFSET = 0;
-
 module.exports = {
   svg2ctx,
 };
@@ -15,11 +13,16 @@ function svg2ctx(src, ctxName='g', indent=0){
 
   var xs = 0, ys = 0;
   var x = 0, y = 0;
+  var xt, yt, tt;
 
   var lines = [];
   var index = 0;
 
-  console.log(d);
+  var prev = null;
+  var next = null;
+
+  var xp = null;
+  var yp = null;
 
   for(var i = 0; i < d.length;){
     while(d[i] === ' ') i++;
@@ -27,7 +30,12 @@ function svg2ctx(src, ctxName='g', indent=0){
     var c = d[i++];
 
     if(/[0-9\-\.]/.test(c)){
-      c = 'l';
+      if(next === null)
+        throw new SyntaxError(`Unrecognized sequence type "${prev}"`);
+
+      c = next;
+      i--;
+
       if(index !== 1 && !rel)
         c = c.toUpperCase();
     }
@@ -35,43 +43,132 @@ function svg2ctx(src, ctxName='g', indent=0){
     var cmd = c.toLowerCase();
     var rel = c === cmd;
 
-    console.log(c, rel);
+    prev = c;
+    next = null;
 
     switch(cmd){
       case 'm':
         [x, y] = coords(rel, 2);
-        xs = x;
-        ys = y;
-        addLine('moveTo', x + OFFSET, y + OFFSET);
+        addLine('beginPath');
+        addLine('moveTo', x, y);
+        next = 'l';
         break;
 
       case 'l':
         [x, y] = coords(rel, 2);
-        addLine('lineTo', x + OFFSET, y + OFFSET);
+        addLine('lineTo', x, y);
+        next = 'l';
         break;
 
       case 'h':
         var [xt] = coords(0, 1);
         if(rel) x += xt;
         else x = xt;
-        addLine('lineTo', x + OFFSET, y + OFFSET);
+        addLine('lineTo', x, y);
+        next = 'h';
         break;
 
       case 'v':
         var [yt] = coords(0, 1);
         if(rel) y += yt;
         else y = yt;
-        addLine('lineTo', x + OFFSET, y + OFFSET);
+        addLine('lineTo', x, y);
+        next = 'v';
+        break;
+
+      case 'a':
+        var [rx, ry, phi, fa, fs, x2, y2] = coords(0, 7);
+        phi *= O.pi / 180;
+
+        var x1 = x;
+        var y1 = y;
+
+        if(rel){
+          x2 += xs;
+          y2 += ys;
+        }
+
+        x = x2;
+        y = y2;
+
+        var cos = Math.cos(phi);
+        var sin = Math.sin(phi);
+
+        xt = (x1 - x2) / 2;
+        yt = (y1 - y2) / 2;
+        var x1p = cos * xt + sin * yt;
+        var y1p = -sin * xt + cos * yt;
+
+        xt = (x1 - x2) / 2;
+        yt = (y1 - y2) / 2;
+        var x1p = cos * xt + sin * yt;
+        var y1p = -sin * xt + cos * yt;
+
+        xt = rx ** 2 * ry ** 2 - rx ** 2 * y1p ** 2 - ry ** 2 * x1p ** 2;
+        yt = rx ** 2 * y1p ** 2 + ry ** 2 * x1p ** 2;
+        tt = Math.sqrt(xt / yt);
+        var sign = fa !== fs ? 1 : -1;
+        var cxp = sign * tt * rx * y1p / ry;
+        var cyp = -sign * tt * ry * x1p / rx;
+
+        xt = (x1 + x2) / 2;
+        yt = (y1 + y2) / 2;
+        var cx = cos * cxp - sin * cyp + xt;
+        var cy = sin * cxp + cos * cyp + yt;
+
+        xt = (x1p - cxp) / rx;
+        yt = (y1p - cyp) / ry;
+        var theta = vecAngle(1, 0, xt, yt);
+        var dtheta = vecAngle(xt, yt, (-x1p - cxp) / rx, (-y1p - cyp) / ry);
+
+        theta = (theta % O.pi2 + O.pi2) % O.pi2;
+        dtheta = (dtheta % O.pi2 + O.pi2) % O.pi2;
+        if(!fs) theta -= O.pi2;
+
+        addLine('ellipse', cx, cy, rx, ry, phi, theta, theta + dtheta, !fs);
+        next = 'a';
+        break;
+
+      case 'c':
+        var [x1, y1, x2, y2, x, y] = coords(rel, 6);
+        addLine('bezierCurveTo', x1, y1, x2, y2, x, y);
+        xp = x * 2 - x2;
+        yp = y * 2 - y2;
+        next = 'c';
+        break;
+
+      case 's':
+        var [x2, y2, x, y] = coords(rel, 4);
+        if(xp === null || yp === null){
+          xp = (x2 + x) / 2;
+          yp = (y2 + y) / 2;
+        }
+        var x1 = xp;
+        var y1 = yp;
+        addLine('bezierCurveTo', x1, y1, x2, y2, x, y);
+        xp = x * 2 - x2;
+        yp = y * 2 - y2;
+        next = 's';
         break;
 
       case 'z':
-        //addLine('closePath');
+        addLine('closePath');
+        addLine('fill');
+        addLine('stroke');
         break;
 
       default:
         throw new SyntaxError(`Unrecognized command "${c}"`);
         break;
     }
+
+    if(cmd !== 'c' && cmd !== 's'){
+      xp = null;
+      yp = null;
+    }
+
+    xs = x;
+    ys = y;
 
     index++;
   }
@@ -101,13 +198,13 @@ function svg2ctx(src, ctxName='g', indent=0){
           continue;
         }
 
-        first = 0;
-
         if(c === '+' || c === '-'){
           if(!first) break;
           first = 0;
           continue;
         }
+
+        first = 0;
 
         if(c === '.'){
           if(dot) break;
@@ -118,7 +215,8 @@ function svg2ctx(src, ctxName='g', indent=0){
         break;
       }
 
-      arr.push(+d.substring(i, --j));
+      j--;
+      arr.push(+d.substring(i, j));
       i = j;
 
       if(arr.length === num)
@@ -126,16 +224,40 @@ function svg2ctx(src, ctxName='g', indent=0){
     }
 
     if(rel){
-      arr[0] += x;
-      arr[1] += y;
+      for(var j = 0; j < num; j += 2){
+        arr[j] += xs;
+        arr[j + 1] += ys;
+      }
     }
 
     return arr;
   }
 
   function addLine(method, ...args){
+    if(lines.length === null)
+      return;
+    args = args.map(arg => {
+      if(typeof arg !== 'number')
+        return arg;
+
+      return +arg.toFixed(5);
+    });
+
     var line = `${ctxName}.${method}(${args.join(', ')});`;
     lines.push(line);
     return line;
   }
+}
+
+function vecAngle(x1, y1, x2, y2){
+  var product = x1 * x2 + y1 * y2;
+  var len1 = vecLen(x1, y1);
+  var len2 = vecLen(x2, y2);
+  var sign = x1 * y2 >= y1 * x2 ? 1 : -1;
+
+  return sign * Math.acos(product / (len1 * len2));
+}
+
+function vecLen(x1, y1){
+  return Math.sqrt(x1 ** 2 + y1 ** 2);
 }
--- svg/main.js
+++ svg/main.js
@@ -1,24 +1,85 @@
 'use strict';
 
+var fs = require('fs');
+var path = require('path');
 var O = require('../framework');
 var media = require('../media');
+var Presentation = require('../presentation');
 var svg = require('.');
+
+var scale = 2;
+var space = 10;
+
+var svgDir = O.dirs.dw;
 
 setTimeout(main);
 
 function main(){
-  var name = 'check';
-  var src = require('fs').readFileSync(require('path').join(O.dirs.dw, `${name}.svg`), 'utf8');
-  var [w, h, code] = svg.svg2ctx(src, 'g');
-  var func = new Function('g', code);
+  var wMax = 0;
+  var hMax = 0;
 
-  console.log(code);
+  var ctxs = fs.readdirSync(svgDir).map(fileName => {
+    var filePath = path.join(svgDir, fileName);
+    var src = fs.readFileSync(filePath, 'utf8');
+    var [w, h, code] = svg.svg2ctx(src);
 
-  media.renderImage('-img/1.png', w, h, (w, h, g) => {
+    w *= scale;
+    h *= scale;
+
+    if(w > wMax) wMax = w;
+    if(h > hMax) hMax = h;
+
+    var g = media.createContext(w, h);
     g.clearRect(0, 0, w, h);
-    g.fillStyle = 'red';
+    g.globalCompositeOperation = 'xor';
+    g.fillStyle = 'black';
+    g.strokeStyle = 'rgba(0,0,0,0)';
+    g.scale(scale, scale);
+
+    var func = new Function('g', code);
     func(g);
-    g.fill();
-    g.stroke();
+
+    return g;
   });
+
+  var w = (wMax + space) * (10 + .5) | 0;
+  var h = (hMax + space) * (18 + .5) | 0;
+
+  var pr = new Presentation(1920, 1080, 60);
+
+  pr.render('-vid/1.mp4', async (w, h, g, g1) => {
+    g1.fillStyle = 'white';
+    g1.fillRect(0, 0, w, h);
+    await pr.fade();
+    await pr.wait(1e3);
+
+    for(var g2 of iconGenerator()){
+      g1.fillStyle = 'white';
+      g1.fillRect(0, 0, w, h);
+      g1.drawImage(g2.canvas, (w - g2.canvas.width) / 2, (h - g2.canvas.height) / 2);
+      await pr.fade();
+    }
+
+    await pr.wait(5e3);
+    await pr.fadeOut();
+  });
+
+  function *iconGenerator(){
+    var g = media.createContext(w, h);
+
+    g.fillStyle = 'white';
+    g.fillRect(0, 0, w, h);
+
+    outer: for(var y = 0, i = 0; 1; y++){
+      for(var x = 0; x < 10; x++, i++){
+        if(i === ctxs.length)
+          break outer;
+
+        var g1 = ctxs[i];
+        g.drawImage(g1.canvas, x * (wMax + space) + (wMax - g1.canvas.width) / 2, y * (hMax + space) + (hMax - g1.canvas.height) / 2);
+
+        yield g;
+      }
+    }
+  }
 }
