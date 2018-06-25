5653344016d162ca86fb41e8e8317a0501f77323
Hakerh400 <hakerh403@gmail.com>
2018-06-25 21:19:25 +0200

    commit

--- grid-detector/main.js
+++ grid-detector/main.js
@@ -11,14 +11,15 @@ var detector = require('.');
 
 var cwd = __dirname;
 
-var url = 'https://user-images.githubusercontent.com/13399152/41868657-0a58860c-78b7-11e8-9102-4ce35e1ba85a.png';
+var url = 'https://user-images.githubusercontent.com/13399152/41870792-55af21dc-78bd-11e8-822a-0cb9bb065d4f.png';
 var inputFile = path.join(tempDir, 'input.png');
 var outputFile = path.join(cwd, 'output.txt');
 
 var s = 40;
+var similarity = .8;
 
 var cols = {
-  wall: conv.color('#00ff00'),
+  wall: conv.color('#808080'),
 };
 
 setTimeout(main);
@@ -27,7 +28,7 @@ async function main(){
   var baseImg = await getBaseImg();
   var tileImg = await getTileImg();
 
-  var grid = await detect(baseImg, tileImg);
+  var grid = await detect(baseImg, tileImg, similarity);
   await save(grid);
 }
 
@@ -38,14 +39,15 @@ async function getTileImg(){
 
   g.fillStyle = cols.wall;
   g.beginPath();
-  g.rect(0, 0, w, h);
+  g.rect(0, 0, w - 1, h - 1);
   g.fill();
+  g.stroke();
 
   return g.canvas;
 }
 
-async function detect(baseImg, tileImg){
-  var grid = detector.detect(baseImg, tileImg);
+async function detect(baseImg, tileImg, similarity){
+  var grid = detector.detect(baseImg, tileImg, similarity);
   return grid;
 }
 
