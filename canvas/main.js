dba69ed3007866bc7c52ac7b65e61d10944596f4
Hakerh400 <hakerh403@gmail.com>
2018-07-01 05:27:15 +0200

    commit

--- canvas/main.js
+++ canvas/main.js
@@ -10,6 +10,7 @@ var h = 480;
 var [wh, hh] = [w, h].map(a => a >> 1);
 
 media.renderImage('-img/1.png', w, h, (w, h, g) => {
+  return;
   var g1 = media.createContext(w, h);
   g.drawImage(g1.canvas, 1, 1, 1, 1, 0, 0, 0, 0);
   g.drawImage(g1.canvas, 0, 0, 0, 0, 1, 1, 1, 1);
