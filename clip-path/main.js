a59bf0888c462939d476e0c8d2e37f1620079f72
Hakerh400 <hakerh403@gmail.com>
2018-10-29 23:42:28 +0100

    commit

--- clip-path/main.js
+++ clip-path/main.js
@@ -10,16 +10,93 @@ const rl = readline.createInterface(process.stdin, process.stdout);
 setTimeout(main);
 
 async function main(){
-  var a = await ask('Top-left');
-  var b = await ask('Top-right');
-  var c = await ask('Bottom-left');
-  var d = await ask('Bottom-right');
+  var arr = await getArr();
+  var flat = O.flatten(arr);
 
-  rl.close();
+  var [ax, ay, bx, by, cx, cy, dx, dy] = flat;
+  // 279 136
+
+  var clipPath = [
+    bx, by,  1, dy,
+    dx,  1, cx, cy,
+     0, ay, ax,
+  ].reduce((clip, cNew, index) => {
+    var coords = O.last(clip).slice();
+    coords[index & 1] = cNew;
+    clip.push(coords)
+    return clip;
+  }, [[ax, 0]]);
+
+  var str = `clip-path: polygon(${
+    clipPath.map(coords => {
+      return coords.map(coord => {
+        return `${Math.round(coord * 100)}%`;
+      }).join(' ')
+    }).join(', ')
+  }) !important;`;
+
+  log(str);
+}
+
+async function getArr(){
+  var arr = await O.caa(4, i => n(i));
+
+  close();
+  return arr;
+
+  async function n(type){
+    var x = type & 1;
+    var y = type >> 1;
+
+    var s1 = y === 0 ? 'Top' : 'Bottom';
+    var s2 = x === 0 ? 'left' : 'right';
+
+    var str = await ask(`${s1}-${s2}`);
+    str = str.trim();
+
+    if(str.length === 0){
+      var [w, h] = [0, 0];
+    }else{
+      var arr = str.split(/\s+/);
+      if(arr.length !== 2) err('Expected 2 numbers');
+
+      arr = arr.map(a => Number(a) / 10);
+      if(arr.some(a => isNaN(a))) err('Invalid number');
+
+      var [w, h] = arr;
+    }
+
+    if(x === 0) w = Math.ceil(w);
+    else w = Math.floor(100 - w);
+
+    if(y === 0) h = Math.ceil(h);
+    else h = Math.floor(100 - h);
+
+    return [w, h].map(a => a / 100);
+  }
 }
 
 function ask(prompt){
   return new Promise(res => {
-    rl.question(prompt, res);
+    rl.question(`${prompt}: `, res);
   });
+}
+
+function close(newLine=1){
+  rl.close();
+  if(newLine) line();
+}
+
+function line(){
+  log('');
+}
+
+function err(msg){
+  line();
+  log(`ERROR: ${msg}`);
+  exit(1);
+}
+
+function exit(code){
+  process.exit(code);
 }
