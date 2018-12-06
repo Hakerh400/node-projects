b110bc2d01556d0d182ff32af71dc0bba2c75d3c
Thomas <hakerh403@gmail.com>
2018-12-06 02:17:33 +0100

    commit

--- hopfield-memory/index.js
+++ hopfield-memory/index.js
@@ -4,38 +4,37 @@ const fs = require('fs');
 const path = require('path');
 const O = require('../framework');
 
-const BIAS = 0;
-
 class HopfieldMemory{
-  constructor(pats){
-    pats = transpose(pats).map(pat => {
-      return uni2bi(pat);
+  constructor(vecs){
+    vecs = transpose(vecs).map(vec => {
+      return uni2bi(vec);
     });
 
-    var len = pats.length;
+    var len = vecs.length;
     var weights = new TriangularMatrix;
 
     for(var j = 0; j !== len; j++)
       for(var i = j + 1; i !== len; i++)
-        weights.set(i, j, dotProd(pats[i], pats[j]));
+        weights.set(i, j, dotProd(vecs[i], vecs[j]));
 
     this.len = len;
     this.weights = weights;
   }
 
-  match(pat){
+  match(vec){
     const {len, weights} = this;
-    if(pat.length !== len) throw new TypeError('Invalid length');
+    if(vec.length !== len) throw new TypeError('Invalid length');
 
-    var x = uni2bi(pat);
+    var x = uni2bi(vec);
     var y = copy(x);
+
+    var indices = O.shuffle(O.ca(len, i => i));
 
     do{
       var upd = 0;
-      var indices = O.shuffle(O.ca(len, i => i));
 
       indices.forEach(i => {
-        var val = x[i] - BIAS;
+        var val = x[i];
 
         for(var j = 0; j !== len; j++)
           val += weights.get(i, j) * y[j];
@@ -69,12 +68,12 @@ function dotProd(v1, v2){
   }, 0);
 }
 
-function uni2bi(pat){
-  return pat.map(val => val === 1 ? 1 : -1);
+function uni2bi(vec){
+  return vec.map(val => val === 1 ? 1 : -1);
 }
 
-function bi2uni(pat){
-  return pat.map(val => val === 1 ? 1 : 0);
+function bi2uni(vec){
+  return vec.map(val => val === 1 ? 1 : 0);
 }
 
 function copy(vec){
