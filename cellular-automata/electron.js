843388bd3bd970f73014d6fc0430a3fd5eb2211b
Thomas <hakerh403@gmail.com>
2019-06-13 01:05:28 +0200

    commit

--- cellular-automata/electron.js
+++ cellular-automata/electron.js
@@ -8,7 +8,7 @@ const ImageData = require('../image-data');
 
 const {abs, round, sin, cos} = Math;
 
-const HD = 1;
+const HD = 0;
 
 const w = HD ? 1920 : 640;
 const h = HD ? 1080 : 480;
@@ -17,7 +17,7 @@ const fast = !HD;
 
 const [wh, hh] = [w, h].map(a => a >> 1);
 
-const duration = 120;
+const duration = 60 * 20;
 const framesNum = fps * duration;
 
 const inputFile = '-dw/1.png';
@@ -26,13 +26,13 @@ const outputFile = getOutputFile(1);
 setTimeout(() => main().catch(log));
 
 async function main(){
-  const size = 5;
+  const size = 3;
   const sizeh = size >> 1;
   const sizes = size ** 2;
   const sizesh = sizes >> 1;
 
-  const col1 = Buffer.alloc(3).fill(0);
-  const col2 = Buffer.alloc(3).fill(255);
+  const col1 = Buffer.from([0, 255, 255]);
+  const col2 = Buffer.from([244, 11, 139]);
 
   let imgd;
   let d1, d2;
@@ -57,33 +57,14 @@ async function main(){
 
       for(let y = 0; y !== h; y++){
         for(let x = 0; x !== w; x++){
-          const prev = d1.get(x, y, 0, 0);
+          const prev = d1.get(x, y, 1, 0);
 
-          let sum = 0;
+          let sum = O.rand(-1, 1);
           for(let j = 0; j != size; j++)
             for(let i = 0; i != size; i++)
               sum += d1.get(x - sizeh + i, y - sizeh + j, 1, 0);
 
           let next = sum > sizesh;
-
-          if(f & 1){
-            const angle = f * O.pi / 200;
-            const xStart = wh + cos(angle) * 100;
-            const yStart = hh + sin(angle) * 100;
-
-            if(O.dist(x, y, xStart, yStart) < 100){
-              const angle = O.pi / 200;
-              const s = sin(angle);
-              const c = cos(angle);
-              const x1 = x - xStart;
-              const y1 = y - yStart;
-              const x2 = round(xStart + x1 * c - y1 * s);
-              const y2 = round(yStart + x1 * s + y1 * c);
-
-              next = d1.get(x2, y2, 1, 0)//sum > sizesh;
-            }
-          }
-
           const bit = next ? 1 : 0;
 
           d2.set(x, y, next);
