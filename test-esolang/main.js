41893689a823c838875e3ec75ffe79e46646cbf1
Thomas <hakerh403@gmail.com>
2019-09-08 03:02:58 +0200

    commit

--- test-esolang/main.js
+++ test-esolang/main.js
@@ -18,9 +18,33 @@ function main(){
   const input = O.rfs(inputFile);
   // const output = esolang(src, input);
 
-  for(let i = 0; i !== 256; i++){
-    const j = esolang(src, Buffer.from([i]))[0];
-    log(String(parseInt(O.rev(i.toString(2).padStart(8, '0')), 2)).padEnd(5), j);
+  const tests = [
+    '', '0', '1',
+    '00', '01', '10', '11',
+    '000', '001', '010', '011',
+    '100', '101', '110', '111',
+  ].concat(O.ca(20, () => {
+    return O.ca(O.randInt(5, .7), () => O.rand(2)).join('');
+  }));
+
+  const func = test => {
+    return test.replace(/[01]/g, a => a ^ 1);
+  };
+
+  for(const test of tests){
+    const expected = func(test);
+    const actual = esolang(src, test).toString();
+    
+    if(actual !== expected){
+      log([
+        ['Input', test],
+        ['Expected', expected],
+        ['Actual', actual],
+      ].map(([a, b]) => {
+        return `${`${a}:`.padEnd(10)} ${b}`;
+      }).join('\n'))
+      break;
+    }
   }
 
   // log(output.toString());
