08c4c43e754e6f9abdcdc2fbda42ab9d8e5dbdd1
Thomas <hakerh403@gmail.com>
2019-12-03 19:56:59 +0100

    commit

---
+++ http-server/main.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const http = require('http');
+const querystring = require('querystring');
+const O = require('../omikron');
+const readline = require('../readline');
+
+const PORT = 8081;
+
+const cwd = __dirname;
+const rootDir = path.join(cwd, '../../browser-projects');
+
+let server = null;
+let rl = null;
+
+const main = () => {
+  server = http.createServer(onReq);
+  server.listen(PORT);
+
+  rl = readline.rl();
+  rl.on('line', onInput)
+};
+
+const onInput = str => {
+  str = str.trim();
+
+  if(str === '') return;
+
+  if(/^q$/i.test(str)){
+    server.close();
+    rl.close();
+    server = null;
+    rl = null;
+    return;
+  }
+
+  log('Unknown command');
+};
+
+const onReq = (req, res) => {
+  const url = new URL(`http://localhost${req.url}`);
+  const urlPath = url.pathname;
+
+  const e404 = () => {
+    res.statusCode = 404;
+    res.end('Page not found');
+  };
+
+  const send = pth => {
+    if(!fs.existsSync(pth)) return 0;
+
+    const stat = fs.statSync(pth);
+
+    if(stat.isFile()){
+      fs.createReadStream(pth).pipe(res);
+      return 1;
+    }
+
+    if(!stat.isDirectory()) return 0;
+    return send(path.join(pth, 'index.htm'));
+  };
+
+  const pth = path.join(rootDir, urlPath);
+  if(!send(pth)) e404();
+};
+
+main();
