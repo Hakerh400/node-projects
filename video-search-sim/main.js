212357bef022aa20493ab3b3f548419362899370
Thomas <hakerh403@gmail.com>
2020-07-14 16:17:51 +0200

    commit

--- video-search-sim/main.js
+++ video-search-sim/main.js
@@ -3,25 +3,28 @@
 const fs = require('fs');
 const path = require('path');
 const O = require('../omikron');
+const arrOrder = require('../arr-order');
 const videoSearchSim = require('.');
+
+const chars = O.chars('A', 26);
 
 const main = () => {
   while(1){
-    const len = O.rand(100);
+    const len = O.rand(10);
     const arr = videoSearchSim.gen(len);
     const lenCalcd = videoSearchSim.calc(arr);
-    if(lenCalcd === len) continue;
+    // if(lenCalcd === len) continue;
 
     const map = new Map();
 
-    log(`Array: ${O.sfa(arr.map(a => {
+    log(`Input: <code>${arr.map(a => {
       if(map.has(a)) return map.get(a);
-      map.set(a, map.size);
-      return map.size - 1;
-    }))}\n`);
+      map.set(a, arrOrder.str(chars, map.size + 1));
+      return arrOrder.str(chars, map.size);
+    }).join(' ')}</code><br/>`);
 
-    log(`Expected: ${len}`);
-    log(`Actual: ${lenCalcd}`);
+    // log(`Expected: ${len}`);
+    log(`Output: <code>${lenCalcd}</code>`);
 
     break;
   }
