bb84b76ae063bac84ecbddf04a02b8e29d376fbe
Thomas <hakerh403@gmail.com>
2020-07-25 21:01:55 +0200

    commit

--- prefix-free-int/main.js
+++ prefix-free-int/main.js
@@ -5,23 +5,80 @@ const path = require('path');
 const O = require('../omikron');
 
 const main = () => {
-  for(let n = 2; n >= 20; n++){
-    log(`Number: ${n}`);
+  O.bion(1);
+
+  for(let m = 2n; m <= 20n; m++){
+    log(`Number: ${m}`);
     log.inc();
 
-    const len1 = log2(n);
-    const len2 = len1 + 2;
-    const num = n - (1 << len1) << 1;
+    for(let n = 0n; n !== m; n++){
+      let bits = '';
+      ser(a => bits += a ? 1 : 0, n, m);
 
-    const lines = [''];
+      let bs = bits;
+      const a = deser(a => [bs[0] | 0, bs = bs.slice(1)][0], m);
 
+      log(`${a} ---> ${bits}`);
+    }
+
+    log.dec();
     log('\n');
   }
 };
 
+const ser = (f, n, m) => {
+  const a = log2(m);
+  const b = 1n << a;
+  const c = (b << 1n) - m;
+
+  if(n < c){
+    for(let i = 0n; i !== a; i++){
+      f(n & 1n);
+      n >>= 1n;
+    }
+  }else{
+    const d = b - c;
+    let e = c + (n - c) % d;
+
+    for(let i = 0n; i !== a; i++){
+      f(e & 1n);
+      e >>= 1n;
+    }
+
+    f(n >= b);
+  }
+};
+
+const deser = (f, m) => {
+  const a = log2(m);
+  const b = 1n << a;
+  const c = (b << 1n) - m;
+  let n = 0n;
+
+  for(let i = 0n; i !== a; i++)
+    if(f()) n |= (1n << i);
+
+  if(n < c) return n;
+
+  const d = b - c;
+  if(f()) n += d;
+
+  return n;
+};
+
 const log2 = n => {
-  for(let i = 0; i !== n; i++)
-    if((1 << i) >= n) return i;
+  let a = 0n;
+  let b = 0n;
+
+  n--;
+
+  while(n){
+    a++;
+    if(!(n & 1n)) b = 1n;
+    n >>= 1n;
+  }
+
+  return a - b;
 };
 
 main();
