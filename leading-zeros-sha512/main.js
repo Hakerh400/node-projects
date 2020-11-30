e4dc4cc6577511ded3501bfb758413212d4b6a82
Thomas <hakerh403@gmail.com>
2020-11-30 15:32:27 +0100

    commit

--- leading-zeros-sha512/main.js
+++ leading-zeros-sha512/main.js
@@ -7,10 +7,11 @@ const O = require('../omikron');
 const findLeadingZeros = require('.');
 
 const chars = '<>+-';
+const start = 1_631_649_791n;
 
 const main = () => {
   const input = ',[.,]';
-  const output = findLeadingZeros(input, chars);
+  const output = findLeadingZeros(input, chars, start);
 
   log(output);
 };
