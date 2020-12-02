7e0f9a5e9a8ba17f20962a595b48a290e07bbdb6
Thomas <hakerh403@gmail.com>
2020-12-02 01:55:34 +0100

    commit

---
+++ bubblegum/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const assert = require('assert');
+const O = require('../omikron');
+const bubblegum = require('.');
+
+const cwd = __dirname;
+const testDir = path.join(cwd, 'test');
+const fin = path.join(test, 'input.txt');
+const fout = path.join(test, 'output.txt');
+
+const main = () => {
+  const
+  bubblegum.encode()
+};
+
+main();
