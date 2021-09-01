8902e5749e20bf5c5454b886535c70c7ac1d1f7b
Hakerh400 <hakerh403@gmail.com>
2021-09-01 15:24:19 +0200

    commit

---
+++ math/theory.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const assert = require('assert');
+const O = require('../omikron');
+
+class Theory{
+
+}
+
+module.exports = Theory;
