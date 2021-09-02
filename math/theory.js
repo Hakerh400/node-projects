e3385a6aeb07e9bef89680864c42e1961928a9c8
Hakerh400 <hakerh403@gmail.com>
2021-09-02 12:05:07 +0200

    commit

--- math/theory.js
+++ math/theory.js
@@ -6,7 +6,40 @@ const assert = require('assert');
 const O = require('../omikron');
 
 class Theory{
+  constructor(parent, name){
+    this.parent = parent;
+    this.name = name;
+  }
 
+  get isDir(){ return 0; }
+  get isFile(){ return 0; }
+
+  hasPranet(){
+    return this.parent !== null;
+  }
+
+  getPath(){
+    const path = [];
+
+    for(let t = this; t !== null; t = t.parent)
+      path.push(t.name);
+
+    return path.reverse();
+  }
+}
+
+class Dir extends Theory{
+  files = new Set();
+
+  get isDir(){ return 1; }
+
+  getFileNames(){
+    return O.sortAsc([...this.files].map(a => a.name));
+  }
+}
+
+class File extends Theory{
+  get isFile(){ return 1; }
 }
 
 module.exports = Theory;
