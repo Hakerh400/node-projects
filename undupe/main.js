bec88742c59e07ca123516635392f662fc2c4cbe
Hakerh400 <hakerh403@gmail.com>
2021-10-12 03:56:12 +0200

    commit

---
+++ undupe/main.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const assert = require('assert');
+const O = require('../omikron');
+const format = require('../format');
+
+const defaultFile = format.path('-dw/1.txt');
+
+const main = () => {
+  const args = process.argv.slice(2);
+  
+  if(args.length === 0)
+    args.push(defaultFile);
+  
+  if(args.length === 0)
+    O.exit('Expected a file path as argument');
+  
+  const pth = args.join(' ');
+  const str = O.rfs(pth, 1);
+  
+  O.wfs(pth, O.sortAsc(O.undupe(O.sanl(str))).join('\n'));
+};
+
+main();
